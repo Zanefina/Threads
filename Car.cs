feb8f6864708b7ea3e4f4c7566861553347f6869
Zanefina Qmega <zanfinaqmega@icloud.com>
2023-05-26 21:55:43 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Threads
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Distance { get; set; }
+        public int Speed { get; set; }
+        public bool Finished { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+            Distance = 0;
+            Speed = 120;
+            Finished = false;
+        }
+
+        public void Drive()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var eventHappened = false;
+
+            while (!Finished && Distance < 1000)
+            {
+                if (!eventHappened && stopwatch.ElapsedMilliseconds >= 10000)
+                {
+                    var random = new Random();
+                    int prob = random.Next(1, 51);
+                    eventHappened = true;
+
+                    switch (prob)
+                    {
+                        case 1: // 1/50 chance of running out of gas
+                            Console.WriteLine($"{Name} ran out of gas and needs to refuel!");
+                            Thread.Sleep(30000);
+                            break;
+                        case 2: // 2/50 chance of getting a flat tire
+                            Console.WriteLine($"{Name} got a flat tire and needs to change it!");
+                            Thread.Sleep(20000);
+                            break;
+                        case 3: // 5/50 chance of bird hitting the windshield
+                        case 4:
+                        case 5:
+                            Console.WriteLine($"{Name} got a bird on its windshield and needs to clean it!");
+                            Thread.Sleep(10000);
+                            break;
+                        case 6: // 10/50 chance of engine trouble
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                            Console.WriteLine($"{Name} has engine trouble and slows down!");
+                            Speed -= 1;
+                            break;
+                    }
+
+                    Distance += Speed;
+                    stopwatch.Restart();
+                    eventHappened = false;
+                }
+                Thread.Sleep(10);
+            }
+            Finished = true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a list of cars
+            var cars = new List<Car>
+        {
+            new Car("Car A"),
+            new Car("Car B"),
+            new Car("Car C"),
+            new Car("Car D")
+        };
+
+            // Create threads for cars
+            var carThreads = cars.Select(car => new Thread(car.Drive)).ToList();
+
+            // Start race
+            Console.WriteLine("Starting race!");
+
+            foreach (var thread in carThreads)
+            {
+                thread.Start();
+            }
+
+            // Wait for cars to finish
+            while (cars.Any(car => car.Distance < 1000 && !car.Finished))
+            {
+                if (Console.KeyAvailable)
+                {
+                    if (Console.ReadLine() == "status")
+                    {
+                        foreach (var car in cars)
+                        {
+                            Console.WriteLine($"{car.Name} - Distance: {car.Distance}m\tSpeed: {car.Speed} km/h");
+                        }
+                    }
+                }
+                Thread.Sleep(100);
+            }
+
+            // Determine positions
+            var positions = new List<string> { "1st", "2nd", "3rd", "4th" };
+            var sortedPositions = positions.Take(cars.Count).ToList();
+            var sortedCars = cars.OrderByDescending(car => car.Distance).ToList();
+
+            for (int i = 0; i < sortedCars.Count; i++)
+            {
+                var car = sortedCars[i];
+                var position = sortedPositions[i];
+                Console.WriteLine($"{position} place: {car.Name} - Distance: {car.Distance}m");
+
+                if (position == "1st")
+                {
+                    Console.WriteLine($"{car.Name} wins the race!");
+                }
+                else if (position == "2nd")
+                {
+                    Console.WriteLine($"{car.Name} takes the 2nd place.");
+                }
+                else if (position == "3rd")
+                {
+                    Console.WriteLine($"{car.Name} finishes in 3rd place.");
+                }
+                else if (position == "4th")
+                {
+                    Console.WriteLine($"{car.Name} finishes in 4th place.");
+                }
+            }
+
+            // Wait for user to exit
+            Console.ReadLine();
+
+
+
+
+            //    // Create cars
+            //    var cars = new List<Car>
+            //    {
+            //    new Car("Car A"),
+            //    new Car("Car B"),
+            //    new Car("Car C"),
+            //    new Car("Car D")
+            //    };
+
+            //    // Create threads for cars
+            //    var carThreads = cars.Select(car => new Thread(car.Drive)).ToList();
+
+            //    // Start race
+            //    Console.WriteLine("Starting race!");
+
+            //    foreach (var thread in carThreads)
+            //    {
+            //        thread.Start();
+            //    }
+
+            //    // Wait for cars to finish
+            //    while (cars.Any(car => car.Distance < 1000))
+            //    {
+            //        if (Console.KeyAvailable)
+            //        {
+            //            if (Console.ReadLine() == "status")
+            //            {
+            //                foreach (var car in cars)
+            //                {
+            //                    Console.WriteLine($"{car.Name} - Distance: {car.Distance}m\tSpeed: {car.Speed} km/h");
+            //                }
+            //            }
+            //        }
+            //        Thread.Sleep(100);
+            //    }
+
+            //    // Determine winner
+            //    var winner = cars.OrderByDescending(car => car.Distance).First();
+            //    Console.WriteLine($"{winner.Name} wins the race!");
+
+            //    // Wait for user to exit
+            //    Console.ReadLine();
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
